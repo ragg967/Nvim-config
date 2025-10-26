2dc2f7f9be64fa07cb15533af9067695e5528cdd
Ragg967 <rywatson1027@gmail.com>
2025-10-26 02:23:25 -0700

    oh yeah

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,3 +9,12 @@ keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', { desc = 'Help tags' }
 -- Oil
 keymap('n', '<leader>e', '<cmd>Oil<cr>', { desc = 'Open file explorer' })
 keymap('n', '-', '<cmd>Oil<cr>', { desc = 'Open parent directory' })
+
+-- LSP
+keymap('n', 'K', vim.lsp.buf.hover, { desc = 'Hover' })
+keymap('n', 'gd', vim.lsp.buf.definition, { desc = 'Go to definition' })
+keymap('n', 'gr', vim.lsp.buf.references, { desc = 'References' })
+keymap('n', '<leader>rn', vim.lsp.buf.rename, { desc = 'Rename' })
+keymap('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'Code action' })
+keymap('n', '[d', vim.diagnostic.goto_prev, { desc = 'Previous diagnostic' })
+keymap('n', ']d', vim.diagnostic.goto_next, { desc = 'Next diagnostic' })
