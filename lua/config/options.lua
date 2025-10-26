c79a34027dff2ffa8ba52b2ee330c50f4b68c72c
Ragg967 <rywatson1027@gmail.com>
2025-10-26 01:52:12 -0700

    he he he ha

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,6 +8,8 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.list = true
 vim.opt.listchars = { tab = '→ ', trail = '·' }
+vim.opt.number = true
+vim.opt.relativenumber = true
 
 -- System clipboard
 vim.opt.clipboard = "unnamedplus"
