d00a365a176453ca0711dc3efbff54dc26a9a912
Ragg967 <rywatson1027@gmail.com>
2025-10-28 21:05:17 -0700

    back up

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,7 +7,6 @@ vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.list = true
-vim.opt.listchars = { tab = '→ ', trail = '·' }
 vim.opt.number = true
 vim.opt.relativenumber = true
 
